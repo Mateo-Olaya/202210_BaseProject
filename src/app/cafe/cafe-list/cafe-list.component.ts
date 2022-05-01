4e9aabe80c01132b68aea352bce297f79de48e18
Mateo-Olaya <m.olaya11@uniandes.edu.co>
2022-04-30 20:01:43 -0500

    Se completa el punto 1

---
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Cafe } from '../cafe';
+import { CafeService } from '../cafe.service';
+
+@Component({
+  selector: 'app-cafe-list',
+  templateUrl: './cafe-list.component.html',
+  styleUrls: ['./cafe-list.component.css']
+})
+export class CafeListComponent implements OnInit {
+
+  cafes: Array<Cafe> = [];
+    constructor(private cafeService: CafeService) { }
+
+  getCafes() {
+    this.cafeService.getCafes().subscribe(cafes => {
+      this.cafes = cafes;
+    });
+  }
+
+  ngOnInit() {
+    this.getCafes();
+  }
+
+}
