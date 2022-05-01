be1a4a07f074ef785c33344397aac7507a8d5f57
Mateo-Olaya <m.olaya11@uniandes.edu.co>
2022-05-01 13:43:17 -0500

    Se finalizan puntos 2 y 3

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,16 +2,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 
 import { CafeListComponent } from './cafe-list.component';
+import { CafeService } from '../cafe.service';
+import { Cafe } from 'src/app/cafe/cafe';
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ CafeListComponent],
+      providers: [CafeService]
+
     })
     .compileComponents();
   }));
@@ -19,10 +27,46 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    component.cafes = [
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+      )
+    ]
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should have 4 rows',() =>{
+    fixture.detectChanges();
+    const quantityCoffee = fixture.debugElement.query(By.css('.table')).nativeElement.rows;
+    expect(quantityCoffee?.length).toEqual(4);
+    });
 });
--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -1,3 +1,4 @@
+import { JsonpClientBackend } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Cafe } from '../cafe';
 import { CafeService } from '../cafe.service';
@@ -10,13 +11,35 @@ import { CafeService } from '../cafe.service';
 export class CafeListComponent implements OnInit {
 
   cafes: Array<Cafe> = [];
+  cuentaBlend: number = 0;
+  cuentaOrigen: number = 0;
+
     constructor(private cafeService: CafeService) { }
 
   getCafes() {
     this.cafeService.getCafes().subscribe(cafes => {
       this.cafes = cafes;
+      this.contarBlend();
     });
   }
+
+  contarBlend(): void {
+
+    let list: string[] = [];
+
+
+    this.cafes.forEach(element => {
+      if(element.tipo == "Blend") {
+        list.push(element.tipo);
+        this.cuentaBlend += 1;
+      }
+      if(element.tipo == "Café de Origen") {
+        list.push(element.tipo);
+        this.cuentaOrigen += 1;
+      }
+    })
+
+   }
 
   ngOnInit() {
     this.getCafes();
